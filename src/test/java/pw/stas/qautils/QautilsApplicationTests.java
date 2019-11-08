85ac067df169ba4f2df46ea49f309ab2814a7544
pythoninja <pythoninja@users.noreply.github.com>
2019-11-08 19:15:42 +0300

    init commit

---
+++ src/test/java/pw/stas/qautils/QautilsApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.stas.qautils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QautilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
