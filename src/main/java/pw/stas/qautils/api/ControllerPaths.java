e7b068abd99b6b1cbae514d0903b8b456cc12840
pythoninja <pythoninja@users.noreply.github.com>
2019-11-09 10:22:42 +0300

    refactor: add paths in api package

---
+++ src/main/java/pw/stas/qautils/api/ControllerPaths.java
@@ -0,0 +1,8 @@
+package pw.stas.qautils.api;
+
+public class ControllerPaths {
+
+  public static final String QA_UTILS_ROOT_PATH = "/api";
+  public static final String QA_UTILS_RANDOM_TEXT_GENERATOR_PATH = "/generators/text";
+
+}
