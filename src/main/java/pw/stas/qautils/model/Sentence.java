43820003537a2283bedcfa0cee1cd869ab877e80
pythoninja <pythoninja@users.noreply.github.com>
2019-11-11 13:19:05 +0300

    feat: add lombok Data and NoArgsConstructor annotations to Sentence model

--- src/main/java/pw/stas/qautils/model/Sentence.java
+++ src/main/java/pw/stas/qautils/model/Sentence.java
@@ -1,43 +1,15 @@
 package pw.stas.qautils.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
 public class Sentence {
 
   private Long sentenceId;
   private String sentence;
   private Integer symbolsCount;
 
-  public Sentence() {
-  }
-
-  public Long getSentenceId() {
-    return sentenceId;
-  }
-
-  public void setSentenceId(Long sentenceId) {
-    this.sentenceId = sentenceId;
-  }
-
-  public String getSentence() {
-    return sentence;
-  }
-
-  public void setSentence(String sentence) {
-    this.sentence = sentence;
-  }
-
-  public Integer getSymbolsCount() {
-    return symbolsCount;
-  }
-
-  public void setSymbolsCount(Integer symbolsCount) {
-    this.symbolsCount = symbolsCount;
-  }
-
-  @Override
-  public String toString() {
-    return "Sentence{" +
-        "id=" + sentenceId +
-        ", sentence='" + sentence + '\'' +
-        '}';
-  }
 }
