e25c5fb799d93ab0cf1676d883b51da031734da8
pythoninja <pythoninja@users.noreply.github.com>
2019-11-11 13:45:55 +0300

    refactor: rename symbolsCount to symbols_count
    Also remove useless sentenceId field

--- src/main/java/pw/stas/qautils/model/Sentence.java
+++ src/main/java/pw/stas/qautils/model/Sentence.java
@@ -1,5 +1,6 @@
 package pw.stas.qautils.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,8 +9,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Sentence {
 
-  private Long sentenceId;
   private String sentence;
+
+  @JsonProperty("symbols_count")
   private Integer symbolsCount;
 
 }
