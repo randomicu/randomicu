5c1bcb6aaf09dc47a4f1293863a9d9c0b4aca5db
pythoninja <pythoninja@users.noreply.github.com>
2019-11-27 22:21:53 +0300

    fix: add production and default values

--- src/main/java/pw/stas/qautils/config/AppConfiguration.java
+++ src/main/java/pw/stas/qautils/config/AppConfiguration.java
@@ -8,10 +8,10 @@ import pw.stas.qautils.util.RandomSentenceGenerator;
 @Configuration
 public class AppConfiguration {
 
-  @Value("${qa-utils.sentence.min-word-length}")
+  @Value("${qa-utils.sentence.min-word-length:2}")
   private Integer minWordLenght;
 
-  @Value("${qa-utils.sentence.max-word-length}")
+  @Value("${qa-utils.sentence.max-word-length:15}")
   private Integer maxWordLenght;
 
 
