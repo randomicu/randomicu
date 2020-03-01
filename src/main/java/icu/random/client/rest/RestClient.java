527e30cc0857f752a7b8c6112dd9b02a3c11ce19
pythoninja <pythoninja@users.noreply.github.com>
2020-03-01 16:58:13 +0300

    refactor: remove DAO layer

--- src/main/java/icu/random/client/rest/RestClient.java
+++ src/main/java/icu/random/client/rest/RestClient.java
@@ -1,7 +1,6 @@
 package icu.random.client.rest;
 
 import kong.unirest.GetRequest;
-import kong.unirest.HttpResponse;
 
 /**
  * Rest client for Lipsum.com API
@@ -12,9 +11,7 @@ public interface RestClient {
   /**
    * Send HTTP GET request to Lipsum.com API
    *
-   * @return {@link HttpResponse} object
+   * @return {@link GetRequest} object
    */
-  <T> HttpResponse<T> get(String path, Class<T> clazz);
-
   GetRequest get(String path);
 }
