a9dee71deac016e1e5ad75f0b49bf50c44fc230e
pythoninja <pythoninja@users.noreply.github.com>
2020-03-18 13:45:02 +0300

    refactor(exceptions): handle MethodArgumentTypeMismatch

--- src/main/java/icu/random/exception/GeneralExceptionHandler.java
+++ src/main/java/icu/random/exception/GeneralExceptionHandler.java
@@ -8,27 +8,10 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class GeneralExceptionHandler {
-
-  @ExceptionHandler(value = NumberFormatException.class)
-  public ResponseEntity<?> handleNumberFormatException(WebRequest request) {
-
-    String message = "Argument should be a number";
-
-    if (!Objects.equals(request.getHeader("Accept"), "application/json")) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
-    }
-
-    GeneralException response = new GeneralException(
-        HttpStatus.BAD_REQUEST.value(),
-        message,
-        System.currentTimeMillis()
-    );
-
-    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-  }
 
   @ExceptionHandler
   public ResponseEntity<?> handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException exc) {
@@ -48,7 +31,28 @@ public class GeneralExceptionHandler {
         exc.getLocalizedMessage(),
         System.currentTimeMillis()
     );
-
     return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
+  }
+
+  @ExceptionHandler
+  public ResponseEntity<?> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException exc, WebRequest request) {
+
+    if (!Objects.equals(request.getHeader("Accept"), "application/json")) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body(String.format("The parameter '%s' of value '%s' could not be converted to type '%s'",
+          exc.getName(),
+          exc.getValue(),
+          Objects.requireNonNull(exc.getRequiredType()).getSimpleName()));
+    }
+
+    GeneralException response = new GeneralException(
+        HttpStatus.BAD_REQUEST.value(),
+        String.format("The parameter '%s' of value '%s' could not be converted to type '%s'",
+            exc.getName(),
+            exc.getValue(),
+            Objects.requireNonNull(exc.getRequiredType()).getSimpleName()),
+        System.currentTimeMillis()
+    );
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
   }
 }
