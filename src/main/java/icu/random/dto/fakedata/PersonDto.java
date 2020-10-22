86779a276f606942f09eda8bffccc05048864527
pythoninja <pythoninja@users.noreply.github.com>
2020-10-22 21:11:51 +0300

    feat(fakedata): extend DTOs for address and person endpoints
    Also add handling of http response (currently for fakedata only) with Unirest library.
    
    Closes #132
    Closes #134

--- src/main/java/icu/random/dto/fakedata/PersonDto.java
+++ src/main/java/icu/random/dto/fakedata/PersonDto.java
@@ -1,15 +1,36 @@
 package icu.random.dto.fakedata;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PersonDto {
-  private String academic_degree;
   private String age;
-  private String avatar;
-  private String blood_type;
+  private String email;
+  private String first_name;
+  private String full_name;
+  private String gender;
+  private String height;
+  private String identifier;
+  private String last_name;
+  private String nationality;
+  private String occupation;
+  private String password;
+  private String political_views;
+  private String telephone;
+  private String title;
+  private String university;
+  private String username;
+  private String weight;
+  private String work_experience;
+  private String inn;
+  private String kpp;
+  private String bic;
+  private String ogrn;
+  private String passport;
 }
---
+++ src/main/java/icu/random/exception/HttpClientFailureException.java
@@ -0,0 +1,9 @@
+package icu.random.exception;
+
+public class HttpClientFailureException extends RuntimeException {
+
+  public HttpClientFailureException(String message) {
+    super(message);
+  }
+
+}
