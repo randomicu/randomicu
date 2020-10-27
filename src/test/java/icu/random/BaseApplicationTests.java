90395be9f0fcdab7a70b0e2595934b0456ae4a7b
pythoninja <pythoninja@users.noreply.github.com>
2020-10-27 19:04:17 +0300

    refactor: rename lipsum-limits to lorem-limits

--- src/test/java/icu/random/BaseApplicationTests.java
+++ src/test/java/icu/random/BaseApplicationTests.java
@@ -11,6 +11,6 @@ abstract public class BaseApplicationTests {
   public static final String RANDOM_SENTENCE_URL = "/api/v1/generators/random-sentence";
   public static final String RANDOM_SENTENCES_URL = "/api/v1/generators/random-sentences";
   public static final String SENTENCE_LIMITS_PATH = "/api/v1/generators/sentence-limits";
-  public static final String LIPSUM_LIMITS_PATH = "/api/v1/generators/lipsum-limits";
+  public static final String LOREM_LIMITS_PATH = "/api/v1/generators/lorem-limits";
 
 }
